139f17e001f8ac8ca175392de35dcfbc9127012a
ian25gts <93157564+ian25gts@users.noreply.github.com>
2022-03-29 21:17:25 +0300

    Update PostmanTest.java

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -20,7 +20,7 @@ public class PostmanTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body("data", equalTo("some data"))
         ;
     }
